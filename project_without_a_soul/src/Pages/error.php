46bb4b3d5f5294b3260475fb03f847aed115642d
WobniarelbuoD <106962703+WobniarelbuoD@users.noreply.github.com>
2022-10-20 12:43:10 +0300

    updated error page.
    Super enhanced error page experience.

--- project_without_a_soul/src/Pages/error.php
+++ project_without_a_soul/src/Pages/error.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <h1>Four Owww Four<h1>
+    <h1 style='position:absolute; left:40%; top:35%'>R Or: Four Owww Four.<h1>
+    <h2 style='position:absolute; left:45%; top:40%'>Peige Nut Finde<h2>
+    <img src="https://media3.giphy.com/media/P7JmDW7IkB7TW/200.gif" cover alt="skeleton" width="100%" height="100%">
 </body>
 </html>
